57414b9fdee5184cdce397074f96a7ace19bbb73
David Beauchamp <david123beauchamp@gmail.com>
2018-05-04 18:55:26 -0600

    Setting up react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
-  </div>
+const Header = () => (
+  <header>
+    <h1>Portfolio</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
+    <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
+  </header>
 );
-
-Header.defaultProps = {
-  title: 'Indecision'
-};
 
 export default Header;
